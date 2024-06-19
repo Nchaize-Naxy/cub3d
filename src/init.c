ebc4e32e20c53575abe0c1952f4707e9155d4461
Noam Chaize--Assba <nchaize-@z3r2p3.42lyon.fr>
2024-06-19 11:02:51 +0200

    pulling

--- src/init.c
+++ src/init.c
@@ -6,7 +6,7 @@
 /*   By: nchaize- <@student.42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/17 14:01:51 by nchaize-          #+#    #+#             */
-/*   Updated: 2024/06/17 14:06:48 by nchaize-         ###   ########.fr       */
+/*   Updated: 2024/06/18 14:25:47 by nchaize-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,20 @@
 
 int	init(t_data *data)
 {
-	data = malloc(sizeof(t_data));
 	data->player = malloc(sizeof(t_player));
 	data->mlx = mlx_init();
+	data->player->pos_x = 80;
+	data->player->pos_y = 80;
+	
+	data->map = malloc(sizeof(char *) * 7);
+	data->map[0] = "111111";
+	data->map[1] = "100001";
+	data->map[2] = "100001";
+	data->map[3] = "100001";
+	data->map[4] = "100001";
+	data->map[5] = "111111";
+	data->map[6] = NULL;
+	
+	
+	return (0);
 }
